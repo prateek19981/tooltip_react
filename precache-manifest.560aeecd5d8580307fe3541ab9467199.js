25ccad17370de1d6e1ca229ccd1856591e1802a6
prateek <gprateek558@gmail.com>
2020-08-09 08:03:20 -0700

    Updates

---
+++ precache-manifest.560aeecd5d8580307fe3541ab9467199.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c1f632a10da0cf82b6c7b2996a5265",
+    "url": "/tooltip_react/index.html"
+  },
+  {
+    "revision": "ac40b6b5da5b584d3a07",
+    "url": "/tooltip_react/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "50b9513f3092a330248c",
+    "url": "/tooltip_react/static/css/main.f3bbc5f7.chunk.css"
+  },
+  {
+    "revision": "ac40b6b5da5b584d3a07",
+    "url": "/tooltip_react/static/js/2.6bced87e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tooltip_react/static/js/2.6bced87e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b9513f3092a330248c",
+    "url": "/tooltip_react/static/js/main.612a83f7.chunk.js"
+  },
+  {
+    "revision": "1bb586b4363037336660",
+    "url": "/tooltip_react/static/js/runtime-main.1b792f8d.js"
+  }
+]);
